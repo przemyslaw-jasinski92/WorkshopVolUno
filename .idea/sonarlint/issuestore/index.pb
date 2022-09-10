4ca70072155bd7c58187c422e9eed60bbfce3ba5
Przemysław Jasiński <przemyslaw.jasinski92@gmail.com>
2022-09-10 13:59:51 +0200

    fixes mistakes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+]
+-src/main/java/pl/coderslab/ConsoleColors.java,3/4/34aed27565361d637bec6c8fac48891e00d3ed5b
--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,6 +1,7 @@
 package pl.coderslab;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.util.Scanner;
 
 public class TaskManager {
 
+
     public static void showList(String[][] tasks) {
         for (int i = 0; i < tasks.length; i++) {
             System.out.print(i + " : ");
@@ -24,15 +26,27 @@ public class TaskManager {
         }
     }
 
-    public static void Menu() throws IOException {
+    public static void showMenu() throws IOException {
         String[][] tasks = loadFileTasks();
         Scanner scanner = new Scanner(System.in);
-        String option = "";
-        while (!option.equals("exit")) {
+        String inputOption = "";
+        while (!inputOption.equals("exit")) {
             System.out.println(ConsoleColors.BLUE + "Please select an option:");
             String[] options = {"add", "remove", "list", "exit"};
             for (String line : options) {
                 System.out.println(ConsoleColors.RESET + line);
+            }
+            inputOption = scanner.nextLine();
+            inputOption = inputOption.toLowerCase();
+            switch (inputOption) {
+                case "add" -> tasks = addTask(tasks);
+                case "remove" -> tasks = removeTask(tasks);
+                case "list" -> showList(tasks);
+                case "exit" -> {
+                    saveToFile(tasks);
+                    System.out.println(ConsoleColors.PURPLE_BOLD_BRIGHT + "Bye, bye:*");
+                }
+                default -> System.out.println("Please select a correct option");
             }
         }
 
@@ -42,24 +56,23 @@ public class TaskManager {
 
         String readLine = "";
         String[] task = new String[0];
-        int countLines = 0;
         Path tasksPath = Paths.get("tasks.csv");
         boolean tasksPathExists = Files.exists(tasksPath);
-        if (tasksPathExists) {
-            for (String line : Files.readAllLines(tasksPath)) {
-                readLine = line;
-                if (!line.equals("")) {
-                    task = Arrays.copyOf(task, task.length + 1);
-                    task[task.length - 1] = readLine;
-                }
-            }
-        } else {
+        if (!tasksPathExists) {
             throw new FileNotFoundException();
         }
+        for (String line : Files.readAllLines(tasksPath)) {
+            readLine = line;
+            if (!line.equals("")) {
+                task = Arrays.copyOf(task, task.length + 1);
+                task[task.length - 1] = readLine;
+            }
+        }
+
         String[][] tasks = new String[task.length][3];
         for (int i = 0; i < tasks.length; i++) {
             String[] splitTasks = task[i].split(",");
-            for (int j = 0; j < tasks[i].length; j++) {
+            for (int j = 0; j < 3; j++) {
                 tasks[i][j] = splitTasks[j];
             }
         }
@@ -73,117 +86,96 @@ public class TaskManager {
                 newListTasks[i][j] = tasks[i][j];
             }
         }
+
+        /*tasks = Arrays.copyOf(tasks, tasks.length + 1);
+        System.out.println(Arrays.toString(tasks));
         Scanner scanner = new Scanner(System.in);
-        String readLine = "";
+        System.out.println("Please add task description:");
+        tasks[tasks.length-1][0] = scanner.nextLine();
+        while (tasks[tasks.length-1][0].contains("&&")) {
+            System.out.println("You put forbidden signs like '&&'. Please add task description without '&&'");
+            tasks[tasks.length-1][0] = scanner.nextLine();
+        }
+        System.out.println("Please add task due date");
+        tasks[tasks.length-1][1] = scanner.nextLine();
+        System.out.println("Is your task is important: true/false");
+        tasks[tasks.length-1][2] = scanner.next();
+        while (!(tasks[tasks.length-1][2].equals("true") || tasks[tasks.length-1][2].equals("false"))) {
+            System.out.println("You chose wrong importance. Enter: true/false:");
+            tasks[tasks.length-1][2] = scanner.next();
+        }*/
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please add task description:");
         newListTasks[tasks.length][0] = scanner.nextLine();
+        while (newListTasks[tasks.length][0].contains("&&")) {
+            System.out.println("You put forbidden signs like '&&'. Please add task description without '&&'");
+            newListTasks[tasks.length][0] = scanner.nextLine();
+        }
         System.out.println("Please add task due date");
         newListTasks[tasks.length][1] = scanner.nextLine();
         System.out.println("Is your task is important: true/false");
-        newListTasks[tasks.length][2] = scanner.nextLine();
+        newListTasks[tasks.length][2] = scanner.next();
+        while (!(newListTasks[tasks.length][2].equals("true") || newListTasks[tasks.length][2].equals("false"))) {
+            System.out.println("You chose wrong importance. Enter: true/false:");
+            newListTasks[tasks.length][2] = scanner.next();
+        }
+
         return newListTasks;
     }
 
     public static String[][] removeTask(String[][] tasks) {
         Scanner scanner = new Scanner(System.in);
+        String checkIndexRemove = "";
         int indexRemove = -1;
         System.out.println("Select number to remove:");
-        indexRemove = scanner.nextInt();
-        while(indexRemove <= 0){
-
-            if(!scanner.hasNextInt()){
+        do {
+            checkIndexRemove = scanner.next();
+            if (!NumberUtils.isParsable(checkIndexRemove)) {
+                System.out.println("Wrong value of index to remove. Enter properly index value:");
                 scanner.nextLine();
-                System.out.println("Incorrect argument passed. Please give number greater or equal 0");
-            }
-            indexRemove = scanner.nextInt();
-            if(indexRemove < 0 || indexRemove >= tasks.length){
-                scanner.next();
-                System.out.println("Incorrect argument passed. Please give number greater or equal 0");
-            }
-        }
-        String[] allTasks;
-        String task = "";
-        for (int i = 0; i < tasks.length; i++) {
-            for (int j = 0; j < tasks[i].length; j++) {
-                if (j == tasks[i].length - 1) {
-                    task += tasks[i][j];
-                } else {
-                    task += tasks[i][j] + ",";
+            } else {
+                indexRemove = Integer.parseInt(checkIndexRemove);
+                if (indexRemove < 0 || indexRemove > tasks.length - 1) {
+                    System.out.println("Index value is out of range. Enter properly index value:");
+                    scanner.nextLine();
                 }
             }
-            task += "&&";
-        }
-        allTasks = task.split("&&");
-        String[][] newListTasks = new String[allTasks.length - 1][3];
-        allTasks = ArrayUtils.remove(allTasks, indexRemove);
-        for (int i = 0; i < newListTasks.length; i++) {
-            String[] splitTask = allTasks[i].split(",");
-            for (int j = 0; j < newListTasks[i].length; j++) {
-                newListTasks[i][j] = splitTask[j];
-            }
-        }
+
+        } while (checkIndexRemove.equals("") || !NumberUtils.isParsable(checkIndexRemove) || indexRemove < 0 || indexRemove > tasks.length - 1);
+        tasks = ArrayUtils.remove(tasks, indexRemove);
         System.out.println("Value was successfully deleted");
-        return newListTasks;
+        return tasks;
     }
 
     public static void saveToFile(String[][] tasks) throws IOException {
         Path tasksPath = Paths.get("tasks.csv");
         boolean tasksPathExists = Files.exists(tasksPath);
-        if (tasksPathExists) {
-            List<String> allTasks = new ArrayList<>();
-            String[] splitTasks = new String[tasks.length];
-            String task = "";
-            for (int i = 0; i < tasks.length; i++) {
-                for (int j = 0; j < tasks[i].length; j++) {
-                    if (j == tasks[i].length - 1) {
-                        task += tasks[i][j];
-                    } else {
-                        task += tasks[i][j] + ",";
-                    }
-                }
-                splitTasks[i] = task;
-                allTasks.add(splitTasks[i]);
-                task = "";
-            }
-            try {
-                Files.write(tasksPath, allTasks);
-            } catch (IOException e) {
-                System.err.println("Cannot save to file");
-            }
-        } else {
+        if (!tasksPathExists) {
             throw new FileNotFoundException();
+        }
+        List<String> allTasks = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tasks.length; i++) {
+            for (int j = 0; j < tasks[i].length; j++) {
+                if (j == tasks[i].length - 1) {
+                    sb.append(tasks[i][j]);
+                } else {
+                    sb.append(tasks[i][j] + ",");
+                }
+            }
+            allTasks.add(sb.toString());
+            sb.delete(0, sb.length());
+        }
+        try {
+            Files.write(tasksPath, allTasks);
+        } catch (IOException e) {
+            System.err.println("Cannot save to file");
         }
     }
 
 
     public static void main(String[] args) throws IOException {
+        showMenu();
 
-        String[][] tasks = loadFileTasks();
-        Scanner scanner = new Scanner(System.in);
-        String inputOption = "";
-        while (!inputOption.equals("exit")) {
-            System.out.println(ConsoleColors.BLUE + "Please select an option:");
-            String[] options = {"add", "remove", "list", "exit"};
-            for (String line : options) {
-                System.out.println(ConsoleColors.RESET + line);
-            }
-            inputOption = scanner.nextLine();
-            switch(inputOption){
-                case "add":
-                    tasks = addTask(tasks);
-                    break;
-                case "remove":
-                    tasks = removeTask(tasks);
-                    break;
-                case "list":
-                    showList(tasks);
-                    break;
-                case "exit":
-                    saveToFile(tasks);
-                    break;
-                default:
-                    System.out.println("Please select a correct option");
-            }
-        }
     }
 }
